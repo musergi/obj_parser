3d8074b3c9fcb24601005f10ad203da9e2797ba8
musergi <sergi.mus@gmail.com>
2020-10-10 08:10:00 +0200

    Added obj content

---
+++ include/obj_parser/face.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include "point.hpp"
+
+class Face {
+public:
+	std::vector<Point> points;
+};
---
+++ include/obj_parser/obj_content.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include "vertex_data.hpp"
+#include "face.hpp"
+
+class ObjContent {
+public:
+	VertexData vertex_data
+	std::vector<Face> faces;
+};
