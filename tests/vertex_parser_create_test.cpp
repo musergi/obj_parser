c555cf2a3110b5726ec1fe669c47bdf8c83d21e9
musergi <sergi.mus@gmail.com>
2020-11-10 20:16:25 +0100

    Swapped to googetest for testing

--- tests/vertex_parser_create_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <obj_parser/vertex_parser.hpp>
-
-int main() {
-    VertexParser vertex_parser1("v 0.0 0.0 0.0 0.0");
-    VertexParser vertex_parser2("vt 2.55555 0.23243 56.3424");
-    VertexParser vertex_parser3("vn 786234.234827542 98234.8902374 8234.234");
-    VertexParser vertex_parser4("vp 98734.42423 87.342434 9824.23434");
-    return 0;
-}
--- tests/vertex_parser_not_empty_on_create_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <obj_parser/vertex_parser.hpp>
-#include <cassert>
-
-int main() {
-    VertexParser vertex_parser("v 0.0 2.0 0.0");
-    assert(vertex_parser.hasFloat() != false);
-    return 0;
-}
